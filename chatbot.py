dcbd4028057524ddfe476f3133bf0606eed17375
atiqaaiman786 <atiqaaimanarain@gmail.com>
2025-05-13 23:14:56 +0500

    Remove notebook-only pip install command

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 import json
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
@@ -26,9 +26,14 @@ index = faiss.IndexFlatL2(embeddings[0].shape[0])
 index.add(np.array(embeddings))
 
 # Load Phi-2 LLM
-tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2")
-model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", device_map="auto")
+#tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2")
+#model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", device_map="auto")
+#generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-rw-1b")
+model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-rw-1b")
 generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
 
 # Retrieval function
 def get_context(query, k=3, threshold=0.75):
